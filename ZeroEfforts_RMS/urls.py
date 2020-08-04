16b7f53be0b70f12f4ec5061e75d0522bf13debd
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-04 23:02:23 +0500

    title name change

--- ZeroEfforts_RMS/urls.py
+++ ZeroEfforts_RMS/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "ZERO EFFORTS"
+admin.site.site_title = "ZERO EFFORTS Admin Portal"
+admin.site.index_title = "Welcome to ZERO EFFOrTS Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
