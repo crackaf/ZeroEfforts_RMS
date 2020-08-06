7eed7ae44107feb7c49a4922e23b619b4f9bc0c8
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-06 22:02:05 +0500

    Remaning changes in new django 3.1

--- pos/admin.py
+++ pos/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-import .models
+from . import models
 
 # Register your models here.
-admin.site.register(home.models.Product)
-admin.site.register(home.models.Brand)
-admin.site.register(home.models.Category)
-admin.site.register(home.models.Customer)
-admin.site.register(home.models.Order)
-admin.site.register(home.models.Order_Item)
-admin.site.register(home.models.Staff)
-admin.site.register(home.models.Stock)
-admin.site.register(home.models.Store)
+admin.site.register(models.Product)
+admin.site.register(models.Brand)
+admin.site.register(models.Category)
+admin.site.register(models.Customer)
+admin.site.register(models.Order)
+admin.site.register(models.Order_Item)
+admin.site.register(models.Staff)
+admin.site.register(models.Stock)
+admin.site.register(models.Store)
