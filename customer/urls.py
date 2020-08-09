478a41b258651cb8e9ab0db9609c8c6f8d9eea24
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-10 04:02:34 +0500

    Customer detail page running

--- customer/urls.py
+++ customer/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "customer"
 urlpatterns = [
     path('', views.customer, name="customer"),
+    path('<int:pk>/', views.customerDetail, name="customer-detail"),
 ]
--- customer/views.py
+++ customer/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import Customer
 # Create your views here.
 
+
 def customer(request):
     customers = Customer.objects.all()
 
     return render(request, 'customer/customers.html', {'customers': customers})
+
+def customerDetail(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    return render(request, 'customer/customer_detail.html', {'customer': customer})
