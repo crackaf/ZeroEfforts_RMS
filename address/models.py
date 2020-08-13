11a0ef98f67b41b0c06b703ebee58eaa11f03aee
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-13 06:44:04 +0500

    Some Changes

--- address/models.py
+++ address/models.py
@@ -5,15 +5,21 @@ from django.db import models
 class Address(models.Model):
     street = models.CharField(
         max_length=255, null=True, blank=True)
-
     country = models.CharField(
         max_length=50, null=True, blank=True)
-
     province = models.CharField(
         max_length=50,null=True, blank=True)
-
     city = models.CharField(
         max_length=50, null=True, blank=True)
-
     zip_code = models.CharField(
         max_length=10,  null=True, blank=True)
+        
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Addresses"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("address:address-detail", kwargs={"pk": self.pk})
