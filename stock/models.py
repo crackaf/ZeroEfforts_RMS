110aa997b614c6a2f7b26bf5aef9f5af2904d393
GHOST HUNT <50262751+ZeroEfforts@users.noreply.github.com>
2020-08-16 12:10:49 +0500

    Stock made

--- stock/models.py
+++ stock/models.py
@@ -9,15 +9,16 @@ class Stock(models.Model):
         Store, on_delete=models.CASCADE)
     inventory = models.ForeignKey(
         Inventory, on_delete=models.CASCADE)
-    quantity=models.IntegerField(
+    quantity = models.IntegerField(
         null=False, blank=False, default=0)
 
     class Meta:
         verbose_name = "Stock"
         verbose_name_plural = "Stocks"
+        unique_together=['store', 'inventory']
 
     def __str__(self):
-        return self.name
+        return (self.store.name+"\t"+self.inventory.name)
 
     def get_absolute_url(self):
         return reverse("stock_detail", kwargs={"pk": self.pk})
